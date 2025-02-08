9b99c344132bbcd2ad0b67fc43de1ee4224c34bf
KerelosKaram <153287134+KerelosKaram@users.noreply.github.com>
2025-02-08 11:37:51 +0200

    Added Login and Register Services

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-
-  constructor() { }
+  constructor(){ }
 
   ngOnInit(): void {
   }
@@ -17,4 +17,7 @@ export class HomeComponent implements OnInit {
     this.registerMode = !this.registerMode;
   }
 
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
 }
